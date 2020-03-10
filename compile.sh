6ea59180b38f03cfd801d37e55f3d09b48b831da
daniel millson <danielmillson923@gmail.com>
2020-03-10 00:57:45 -0400

    Fixed script because Cameron added a stupid space in his directory name AAAARRRRRHHHHHH

--- compile.sh
+++ compile.sh
@@ -3,23 +3,24 @@
 
 dir="./"
 
-for d in $dir*
+for d in "./Chapter"*
 do
+    echo $d
     if [ -d "$d" ]
     then
-        cd $d
+        cd "$d"
         for dot in *.dot
         do
             file=${dot%".dot"}
             if [[ $dot == n-* ]]
             then
                 file="${file#"n-"}.pdf"
-                echo "Compiling: $dot - $file"
+                echo "Compiling (neato): $dot - $file"
                 neato -Tpdf $dot -o $file
             elif [[ $dot == c-* ]]
             then
                 file="${file#"c-"}.pdf"
-                echo "Compiling: $dot - $file"
+                echo "Compiling (circo): $dot - $file"
                 circo -Tpdf $dot -o $file
             else
                 echo "Error compiling $dot: unrecognized DOT file prefix (use n- or c-)"
@@ -30,6 +31,6 @@ do
 done
 
 pdflatex cmsc420-notes.tex
-bibtex cmsc420-notes
+bibtex ./cmsc420-notes.aux
 pdflatex cmsc420-notes.tex
 pdflatex cmsc420-notes.tex
